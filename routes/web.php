8977fe81457f6aea4e0799464b8a014d3301da9e
hildah <jelagathildah@gmail.com>
2021-01-22 12:47:52 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/',[App\http\Controllers\FormController::class,'index']);
-Route::post('index',[App\http\Controllers\FormController::class,'store'])->name('store');
-Route::post('/',[App\http\Controllers\FormController::class,'index'])->name('index');
-Route::get('/index2',[App\http\Controllers\FormController::class,'index2'])->name('index2');
-Route::get('/requisitions/{id}',[App\http\Controllers\FormController::class,'requisitions'])->name('requisitions');
+Route::get('/', 'FormController@index');
+Route::post('index', 'FormController@store')->name('store');
+Route::post('/', 'FormController@index')->name('index');
+Route::get('/index2', 'FormController@index2')->name('index2');
+Route::get('/requisitions/{id}', 'FormController@requisitions')->name('requisitions');
 
-Route::get('/approve/{id}', [App\http\Controllers\FormController::class,'approveReq'])->name('approveReq');
-Route::get('/disapprove/{id}', [App\http\Controllers\FormController::class,'disapproveReq'])->name('disapproveReq');
+Route::get('/approve/{id}',  'FormController@approveReq')->name('approveReq');
+Route::get('/disapprove/{id}',  'FormController@disapproveReq')->name('disapproveReq');
 
-Route::get('/sendEmail', [App\http\Controllers\FormController::class, 'sendEmail'])->name('sendEmail');
+Route::get('/sendEmail',  'FormController@sendEmail')->name('sendEmail');
