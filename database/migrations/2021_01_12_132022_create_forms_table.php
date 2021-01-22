527a72ef7aec621d93470b92df2e4845960a8745
hildah <jelagathildah@gmail.com>
2021-01-22 12:35:21 +0300

    commit

--- database/migrations/2021_01_12_132022_create_forms_table.php
+++ database/migrations/2021_01_12_132022_create_forms_table.php
@@ -14,7 +14,7 @@ class CreateFormsTable extends Migration
     public function up()
     {
         Schema::create('forms', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('department');
             $table->string('email');
